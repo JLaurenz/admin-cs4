5e8f9bc98291945ffe106bcb1d9861c6cb1526cc
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2022-05-23 05:52:59 +0800

    c

--- src/pages/RidersList.jsx
+++ src/pages/RidersList.jsx
@@ -7,7 +7,7 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Sidebar from '../components/Sidebar';
-import '../Styles/home.css';
+import '../Styles/Home.css';
 
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
