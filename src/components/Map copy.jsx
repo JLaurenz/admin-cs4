722beedf71763a77feb8f6d24debd004d97b1c1f
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2022-05-25 05:23:09 +0800

    back up

--- src/components/Map copy.jsx
+++ src/components/Map copy.jsx
@@ -1,73 +1,47 @@
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { getDatabase, ref, onValue} from "firebase/database";
-import 'firebase/database';
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2VhbXN1cXIiLCJhIjoiY2wxOXc4Y3QwMTIzazNqbnd3ZXYyNmZsMyJ9.8QvIQjNW74qt9aE6K-6b7A';
 
-export default function Home() {
-
-    const mapContainer = useRef(null);
-    const map = useRef(null);
-    const [lng, setLng] = useState('');
-    const [lat, setLat] = useState('');
-    const [zoom, setZoom] = useState('');
-    const coordinate =[];
-    const db = getDatabase();
-    const ref = db.ref('/');
-
-    
-
-    useEffect(() => {
-        if (map.current) return; 
-        map.current = new mapboxgl.Map({
-        container: mapContainer.current,
-        style: 'mapbox://styles/mapbox/streets-v11',
-        center: [121.5010, 14.3507],
-        zoom: 11
-        });
-    });
-
-    setInterval(() => {
-        // if coordinate status is offline, create a new marker
-        coordinate.forEach(coordinate => {
-          if (coordinate.hasOwnProperty('status') && coordinate.status === 'offline') {
-            const {title, description, 'marker-color': color, 'marker-size': size, 'marker-symbol': symbol} = coordinate.properties;
-            const newMarker = new mapboxgl.Marker({
-              color,
-              size,
-              symbol
-            })
-            .setLngLat(coordinate.geometry.coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 })
-              .setHTML(`<h3>${title}</h3><p>${description}</p>`))
-            .addTo(map.current);
-            ListMarker.push(newMarker);
-            coordinate.properties.status = 'Online';
-          }
-          else {
-            ListMarker[title].setLngLat(coordinate.geometry.coordinates)
-            .setPopup(new mapboxgl.Popup({ offset: 25 })
-              .setHTML(`<h3>${title}</h3><p>${description}</p>`))
-            .addTo(map.current);
-          }
-        });
-      }, 1000);
-    
-
-    useEffect(() => {
-        if (!map.current) return;
-        map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
+  const dbRef1 = ref(getDatabase());
+  get(child(dbRef1, `/Online Riders/1234`)).then((snapshot) => {
+    const data = snapshot.val();
+    const keys = Object.keys(data);
+    keys.forEach(key => {
+      const obj = data[key];
+      coordinate.push({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [obj.Longitude, obj.Latitude]
+        },
+        properties: {
+          title: obj.Name,
+          description: obj.Duration,
+          status: obj.Status,
+          color: obj.color
+        }
       });
+      const newMarker = new mapboxgl.Marker({
+        color: obj.color,
+        size: 'small',
+        symbol: 'marker'
+      })
+      newMarker.setLngLat([obj.Longitude, obj.Latitude])
+      newMarker.setPopup(new mapboxgl.Popup({ offset: 25 })
+        .setHTML(`<h3>${obj.Name}</h3><p>${obj.Duration}</p>`))
+      newMarker.addTo(map.current);
+      ListMarker[obj.Name] = newMarker;
     });
-
-    return (
-        <div className="container">
-            <div className="content">
-                <div ref={mapContainer} className="map-container" />
-            </div>
-        </div>
-    )
-} 
+  });
+ 
+  onValue(dbRef1, (snapshot => {
+    const data = snapshot.val();
+    const keys = Object.keys(data);
+    setInterval(() => {
+    keys.forEach(key => {
+      if (ListMarker.hasOwnProperty(obj.Name)) {
+        ListMarker[obj.Name].setLngLat([obj.Longitude, obj.Latitude])
+        ListMarker[obj.Name].setPopup(new mapboxgl.Popup({ offset: 25 })
+          .setHTML(`<h3>${obj.Name}</h3><p>${obj.Duration}</p>`))
+          ListMarker[obj.Name].addTo(map.current);
+      }
+    }, 1000);
+    });
+  }));
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import { getDatabase, ref, onValue, DataSnapshot, set} from "firebase/database";
+import { getDatabase, ref, onValue, DataSnapshot, set, child, get} from "firebase/database";
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2VhbXN1cXIiLCJhIjoiY2wxOXc4Y3QwMTIzazNqbnd3ZXYyNmZsMyJ9.8QvIQjNW74qt9aE6K-6b7A';
 
 export default function Home() {
@@ -10,12 +10,16 @@ export default function Home() {
   const [lng, setLng] = useState('');
   const [lat, setLat] = useState('');
   const [zoom, setZoom] = useState('');
+
+  const linelocation = [];
   
   // create an array of random generated color
   const ListMarker = [];
   const db = getDatabase();
   const coordinate =[];
+  const linetrack = [];
   const dbRef = ref(db, '/Online Riders'+ '/1234');
+
   onValue(dbRef, (snapshot) => {
     const data = snapshot.val();
     const userIds = Object.keys(data);
@@ -34,7 +38,8 @@ export default function Home() {
             description: Duration,
             'marker-color': color,
             'marker-size': 'medium',
-            'marker-symbol': 'marker'
+            'marker-symbol': 'marker',
+            'status': 'Online'
           }
         });
       }
@@ -44,28 +49,31 @@ export default function Home() {
   setInterval(() => {
     coordinate.forEach(coordinate => {
       const {title, description, 'marker-color': color, 'marker-size': size, 'marker-symbol': symbol} = coordinate.properties;
+      if (coordinate.hasOwnProperty('status') && coordinate.status === 'offline') { return; }
       if (ListMarker.hasOwnProperty(title)) {
-        ListMarker[title].remove();
-        ListMarker[title].setLngLat(coordinate.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({ offset: 25 })
-          .setHTML(`<h3>${title}</h3><p>${description}</p>`))
-        .addTo(map.current);
-        ListMarker[title] = newMarker;
+
+        const marker = ListMarker[title];
+        const markerCoordinate = marker.getLngLat();
+        if (markerCoordinate.lat !== coordinate.geometry.coordinates[1] || markerCoordinate.lng !== coordinate.geometry.coordinates[0]) {
+          marker.setLngLat(coordinate.geometry.coordinates)
+          marker.setPopup(new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h3>${title}</h3><p>${description}</p>`))
+          marker.addTo(map.current);
+        }
       }
       else {
-        const newMarker = new mapboxgl.Marker({
-          color,
-          size,
-          symbol
-        })
-        .setLngLat(coordinate.geometry.coordinates)
-        .setPopup(new mapboxgl.Popup({ offset: 25 })
+        const newMarker = new mapboxgl.Marker({ color, size, symbol })
+        linelocation[title] = [coordinate.geometry.coordinates];
+        newMarker.setLngLat(coordinate.geometry.coordinates)
+        newMarker.setPopup(new mapboxgl.Popup({ offset: 25 })
           .setHTML(`<h3>${title}</h3><p>${description}</p>`))
-        .addTo(map.current);
+        newMarker.addTo(map.current);
         ListMarker[title] = newMarker;
+        
       }
     });
   }, 1000);
+
 
   useEffect(() => {
     if (map.current) return; 
@@ -74,6 +82,8 @@ export default function Home() {
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [121.5010, 14.3507],
     zoom: 11
+
+    
     });
 
     if (!map.current) return;
@@ -81,7 +91,20 @@ export default function Home() {
     setLng(map.current.getCenter().lng.toFixed(4));
     setLat(map.current.getCenter().lat.toFixed(4));
     setZoom(map.current.getZoom().toFixed(2));
+
     });
+
+    const base1 = new mapboxgl.Marker({
+      color: '#ff0000',
+      size: 'large',
+      symbol: 'home'
+    });
+    base1.setLngLat([121.41402295374215 , 14.280144331354549]);
+    base1.setPopup(new mapboxgl.Popup({ offset: 25 })
+      .setHTML(`<h3>J&T</h3><p>Sta Cruz</p>`))
+    base1.addTo(map.current);
+
+
   });
 
   return (
