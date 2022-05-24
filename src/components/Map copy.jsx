66ca5644f71229bd6594e5936d355eebf2da4994
John Laurenz Alvendia <88194555+JLaurenz@users.noreply.github.com>
2022-05-25 06:14:34 +0800

    moving but no line

--- src/components/Map copy.jsx
+++ src/components/Map copy.jsx
@@ -18,30 +18,61 @@
           color: obj.color
         }
       });
-      const newMarker = new mapboxgl.Marker({
-        color: obj.color,
-        size: 'small',
-        symbol: 'marker'
-      })
-      newMarker.setLngLat([obj.Longitude, obj.Latitude])
-      newMarker.setPopup(new mapboxgl.Popup({ offset: 25 })
-        .setHTML(`<h3>${obj.Name}</h3><p>${obj.Duration}</p>`))
-      newMarker.addTo(map.current);
-      ListMarker[obj.Name] = newMarker;
     });
   });
- 
-  onValue(dbRef1, (snapshot => {
+
+
+
+  
+  onValue(dbRef, (snapshot) => {
     const data = snapshot.val();
-    const keys = Object.keys(data);
-    setInterval(() => {
-    keys.forEach(key => {
-      if (ListMarker.hasOwnProperty(obj.Name)) {
-        ListMarker[obj.Name].setLngLat([obj.Longitude, obj.Latitude])
-        ListMarker[obj.Name].setPopup(new mapboxgl.Popup({ offset: 25 })
-          .setHTML(`<h3>${obj.Name}</h3><p>${obj.Duration}</p>`))
-          ListMarker[obj.Name].addTo(map.current);
+    const userIds = Object.keys(data);
+    userIds.forEach(userId => {
+      const user = data[userId];
+      const {Duration, Latitude, Longitude, Name, color } = user;
+      if (Latitude !== undefined && Longitude !== undefined) {
+        coordinate.push({
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [Longitude, Latitude]
+          },
+          properties: {
+            title: Name,
+            description: Duration,
+            'marker-color': color,
+            'marker-size': 'medium',
+            'marker-symbol': 'marker',
+            'status': 'Online'
+          }
+        });
       }
-    }, 1000);
     });
-  }));
+  });
+
+  setInterval(() => {
+    coordinate.forEach(coordinate => {
+      const {title, description, 'marker-color': color, 'marker-size': size, 'marker-symbol': symbol} = coordinate.properties;
+      if (coordinate.hasOwnProperty('status') && coordinate.status === 'offline') { return; }
+      if (ListMarker.hasOwnProperty(title)) {
+        // check if coordinate changed
+        const marker = ListMarker[title];
+        const markerCoordinate = marker.getLngLat();
+        if (markerCoordinate.lat !== coordinate.geometry.coordinates[1] || markerCoordinate.lng !== coordinate.geometry.coordinates[0]) {
+          marker.setLngLat(coordinate.geometry.coordinates)
+          marker.setPopup(new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h3>${title}</h3><p>${description}</p>`))
+          marker.addTo(map.current);
+        }
+      }
+      else {
+        const newMarker = new mapboxgl.Marker({ color, size, symbol })
+        linelocation[title] = [coordinate.geometry.coordinates];
+        newMarker.setLngLat(coordinate.geometry.coordinates)
+        newMarker.setPopup(new mapboxgl.Popup({ offset: 25 })
+          .setHTML(`<h3>${title}</h3><p>${description}</p>`))
+        newMarker.addTo(map.current);
+        ListMarker[title] = newMarker;
+      }
+    });
+  }, 1000);
